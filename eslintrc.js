e280500ca6f85854ce5b609ad6f71ddb60bac089
longlonggi <985159006@qq.com>
2021-08-04 08:55:39 +0800

    Create eslintrc.js

---
+++ eslintrc.js
@@ -0,0 +1,13 @@
+{
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+   "vetur.format.defaultFormatter.html": "prettier",
+   "vetur.format.defaultFormatter.css": "prettier",
+   "vetur.format.defaultFormatter.postcss": "prettier",
+   "vetur.format.defaultFormatter.scss": "prettier",
+   "vetur.format.defaultFormatter.less": "prettier",
+   "vetur.format.defaultFormatter.stylus": "stylus-supremacy",
+   "vetur.format.defaultFormatter.ts": "prettier",
+   "vetur.format.defaultFormatter.sass": "sass-formatter",
+   "vetur.format.defaultFormatter.js": "vscode-typescript",
+   "javascript.format.semicolons": "remove"
+}
