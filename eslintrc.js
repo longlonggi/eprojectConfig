383f36480e7b5e73b9c8e707a96ebaeaae9b5b78
longlonggi <985159006@qq.com>
2021-08-04 09:29:05 +0800

    Update eslintrc.js

--- eslintrc.js
+++ eslintrc.js
@@ -9,5 +9,42 @@
    "vetur.format.defaultFormatter.ts": "prettier",
    "vetur.format.defaultFormatter.sass": "sass-formatter",
    "vetur.format.defaultFormatter.js": "vscode-typescript",
-   "javascript.format.semicolons": "remove"
+   "javascript.format.semicolons": "remove",
+   "javascript.format.insertSpaceBeforeFunctionParenthesis": false,
+   "editor.codeActionsOnSave": {
+     "source.fixAll": true
+   },
+   // 指定 *.vue 文件的格式化工具为vetur
+   "[vue]": {
+     "editor.defaultFormatter": "octref.vetur"
+   },
+   // 指定 *.js文件的格式化工具为vscode自带
+   "[javascript]": {
+     "editor.defaultFormatter": "vscode.typescript-language-features"
+   },
+   "vetur.format.defaultFormatterOptions": {
+     "JS-beautify-HTML": {
+       "wrap_attributes": "force-aligned"
+     },
+     "prettyhtml": {
+       "printWidth": 120,
+       "singleQuote": false,
+       "wrapAttributes": false,
+       "sortAttributes": true
+     },
+     "prettier": {
+       "semi": false,
+       "singleQuote": true,
+       "eslintIntegration": true,
+       "arrowParens": "always"
+     }
+   },
+   "editor.detectIndentation": false,
+   "editor.tabSize": 2,
+   "editor.formatOnSave": true,
+   "diffEditor.ignoreTrimWhitespace": false,
+   "[html]": {
+      "editor.defaultFormatter": "vscode.html-language-features"
+   },
+   "[json]"
 }
